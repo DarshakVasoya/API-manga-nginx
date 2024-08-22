138930ff6adc53eb64a3e095d8b8ef02c8821a92
Darshak <darshakvasoya1310@gmail.com>
2024-08-22 05:32:29 +0000

    done

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def list_manga():
 # Run the Flask app
 if __name__ == "__main__":
     # app.run(host="0.0.0.0", port=5000, debug=True)
-    app.run(debug=True,host='0.0.0.0')
+    app.run(host='0.0.0.0',debug=True)
